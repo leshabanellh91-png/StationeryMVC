7d8fdc7508b9cad2d943af24fc178ae0743bf572
LERATO LESHABANE <leshabanellh91@gmail.com>
2026-01-14 10:56:59 +0200

    added statitioneryItem class inside models folder, added stationerycontroller class inside controller and also updated edit.cshtml, create.cshtml, index.cshtml.

--- StationeryMVC/Controllers/StationeryController.cs
+++ StationeryMVC/Controllers/StationeryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StationeryMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StationeryMVC.Controllers
 {
@@ -7,8 +9,17 @@ namespace StationeryMVC.Controllers
     {
         private static List<StationeryItem> items = new()
         {
-            new StationeryItem { Id = 1, Name = "Pen", Quantity = 10, Price = 5 },
-            new StationeryItem { Id = 2, Name = "Notebook", Quantity = 5, Price = 25 }
+            new StationeryItem { Id = 1, Name = "Pen", Category = "Writing", Quantity = 10, Price = 5 },
+            new StationeryItem { Id = 2, Name = "Notebook", Category = "Books", Quantity = 5, Price = 25 }
+        };
+
+        private static readonly List<string> categories = new()
+        {
+            "Writing",
+            "Books",
+            "Office",
+            "Art",
+            "Other"
         };
 
         public IActionResult Index()
@@ -16,39 +27,25 @@ namespace StationeryMVC.Controllers
             return View(items);
         }
 
+        // GET
         public IActionResult Create()
         {
+            ViewBag.Categories = categories;
             return View();
         }
 
+        // POST
         [HttpPost]
         public IActionResult Create(StationeryItem item)
         {
-            item.Id = items.Max(i => i.Id) + 1;
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = categories;
+                return View(item);
+            }
+
+            item.Id = items.Any() ? items.Max(i => i.Id) + 1 : 1;
             items.Add(item);
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Edit(int id)
-        {
-            var item = items.FirstOrDefault(i => i.Id == id);
-            return View(item);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(StationeryItem item)
-        {
-            var existing = items.First(i => i.Id == item.Id);
-            existing.Name = item.Name;
-            existing.Quantity = item.Quantity;
-            existing.Price = item.Price;
-            return RedirectToAction(nameof(Index));
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var item = items.First(i => i.Id == id);
-            items.Remove(item);
             return RedirectToAction(nameof(Index));
         }
     }
--- StationeryMVC/Models/StationeryItem.cs
+++ StationeryMVC/Models/StationeryItem.cs
@@ -6,11 +6,21 @@ namespace StationeryMVC.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Item name is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, 1000)]
         public int Quantity { get; set; }
 
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 10000)]
         public decimal Price { get; set; }
+        public decimal TotalPrice => Quantity * Price;
+
+        
     }
 }
